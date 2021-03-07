add271b15aceb43f45b65bdb2ab4f178f031c326
natashta <natashta@gmail.com>
2021-03-07 23:28:58 +0300

    feat:add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    throw new CustomError('Not implemented');
+  }
+  let teamName = [];
+  members.forEach((el) => {
+    if (typeof el === 'string') {
+      teamName.push(el.trim()[0].toUpperCase());
+    }
+  });
+  return teamName.sort().join('');
 };
