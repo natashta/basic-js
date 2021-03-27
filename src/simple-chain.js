3e08cfcc540863c6ca6508ebc2a9ec5d353830c5
natashta <natashta@gmail.com>
2021-03-27 19:21:25 +0300

    fix: fixed some error throwing

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -3,29 +3,29 @@ const CustomError = require("../extensions/custom-error");
 const chainMaker = {
   chain: [],
   getLength() {
-    if (!this.chain.length) throw new CustomError('Not implemented');
+    if (!this.chain.length) return false;
     return this.chain.length;
   },
   addLink(value) {
-    if (!value) throw new CustomError('Not implemented');
+    if (value === undefined) value = '';
     this.chain.push("( " + value + " )");
     return this;
   },
   removeLink(position) {
   if (isNaN(position) || position < 0 || position > this.chain.length){
       this.chain = [];
-      throw new CustomError('Not implemented');
+      throw new Error('Error');
     }
       this.chain.splice(position - 1, 1)
       return this;
   },
   reverseChain() {
-    if (!this.chain.length) throw new CustomError('Not implemented');
+    
     this.chain.reverse();
     return this;
   },
   finishChain() {
-    if (!this.chain.length) throw new CustomError('Not implemented');
+    
     let result = this.chain.join("~~");
     this.chain = [];
     return result;
